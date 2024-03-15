07aeed2d1e60494cfb368d81a1c9ecc711b98c5b
Shashwat Singh <singhshashwat1002@gmail.com>
2024-03-15 13:17:27 +0530

    initialization of the study model

--- scripts/utils/model_init.py
+++ scripts/utils/model_init.py
@@ -1,6 +1,8 @@
 from typing import *
 from torch.nn import Module
 from torch import nn
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPConfig
+from transformers import AutoModel, AutoTokenizer, AutoModel, AutoConfig
 
 
 def probe_block(input_dim: int, output_dim: int, non_linearity: str) -> Module:
@@ -27,5 +29,50 @@ def probe_model(
         last_hidden = hidden_dim
     layers.append(nn.Linear(last_hidden, output_dim))
     return nn.Sequential(*layers)
+
+
+def init_subject_model(
+    model_name: str, model_type: str, model_config=None, device: str = "cpu"
+) -> dict:
+    """
+    To initialize the subject model (the one being studied)
+    Inputs:
+    * model_name: str: name of the model (as in huggingface)
+    * model_type: str: type of the model (Text Encoder or image)
+    * model_config: Config object of the model
+    Outputs:
+    dict: dictionary containing the model and the config and related
+    """
+
+    if model_type == "text":
+        if model_config is None:
+            model_config = AutoConfig.from_pretrained(model_name)
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModel.from_pretrained(model_name, config=model_config)
+        model.to(device=device)
+        model.eval()
+        return {
+            "model_text": model,
+            "tokenizer": tokenizer,
+            "config_text": model_config,
+        }
+    elif model_type == "clip":
+        if model_config is None:
+            model_config = CLIPConfig.from_pretrained(model_name)
+        tokenizer = CLIPTokenizer.from_pretrained(model_name)
+        model = CLIPModel.from_pretrained(model_name, config=model_config)
+        model.to(device=device)
+        model.eval()
+        model_text = model.text_model
+        text_config = model_text.config
+        return {
+            "model_text": model_text,
+            "tokenizer": tokenizer,
+            "config_text": text_config,
+            "model": model,
+            "config": model_config,
+        }
+    else:
+        raise ValueError("Model type not recognized")
 
 

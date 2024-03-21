5c6b486137822113b52599ae44e7927fb2218f26
Shashwat Singh <singhshashwat1002@gmail.com>
2024-03-21 23:05:00 +0530

    add padding function for the dep parse task

--- scripts/utils/data_utils.py
+++ scripts/utils/data_utils.py
@@ -3,7 +3,7 @@ import torch
 from typing import *
 
 
-def deal_with_double_punctuation_case(tokenlist: TokenList):
+def deal_with_double_punctuation_case(tokenlist: TokenList, task: str):
     """
     This function deals with the case where there are two punctuation marks in a row.
     For example '."'
@@ -12,6 +12,11 @@ def deal_with_double_punctuation_case(tokenlist: TokenList):
     """
     new_token_list = []
     last_is_punct = False
+
+    # this is because indexing is changing and we have to fix that for the dependency parsing stuff
+    index_map = []
+    # print("hi")
+
     for i, token in enumerate(tokenlist):
         if last_is_punct:
             if token["upos"] == "PUNCT":
@@ -20,11 +25,27 @@ def deal_with_double_punctuation_case(tokenlist: TokenList):
                     new_token_list[-1]["lemma"] + token["lemma"]
                 )
                 new_token_list[-1]["misc"] = token["misc"]
+                index_map.append(index_map[-1] - 1)
             else:
                 new_token_list.append(token)
+                index_map.append(index_map[-1])
         else:
             new_token_list.append(token)
+            if len(index_map) != 0:
+                index_map.append(index_map[-1])
+            else:
+                index_map.append(0)
         last_is_punct = token["upos"] == "PUNCT"
+    # print(index_map)
+    # remap the dependency parses
+    if task == "dep_parse":
+        assert len(tokenlist) == len(index_map)
+        for i, token in enumerate(new_token_list):
+            if token["head"] is not None:
+                assert token["head"] < len(tokenlist)
+                token["head"] = index_map[token["head"]] + token["head"]
+
+                token["deps"] = [(token["deprel"], token["head"])]
     return new_token_list
 
 
@@ -58,3 +79,33 @@ def pool_for_words(
             word_embedding = word_embedding.max(dim=0).values
         pooled.append(word_embedding)
     return torch.stack(pooled)
+
+
+from torch.nn.utils.rnn import pad_sequence
+
+
+def custom_embedding_padding(
+    batch: List[Tuple[torch.Tensor, torch.Tensor]],
+    padding_value_x: float = 0.0,
+    padding_value_y: int = -1,
+):
+    """
+    Inputs:
+    Tuple: (embedding_list: List of torch tensors of shape (seq_len, d)
+    y_list: List of torch tensors of shape (seq_len,))
+    padding_value_x: float
+    padding_value_y: int
+    Outputs:
+    padded_x: torch tensor of shape (batch_size, max_seq_len, d)
+    padded_y: torch tensor of shape (batch_size, max_seq_len)
+    """
+
+    embedding_list = [x[0] for x in batch]
+    y_list = [x[1] for x in batch]
+    padded_x = pad_sequence(
+        embedding_list, batch_first=True, padding_value=padding_value_x
+    )
+
+    padded_y = pad_sequence(y_list, batch_first=True, padding_value=padding_value_y)
+
+    return padded_x, padded_y
